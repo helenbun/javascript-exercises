f0c82db3faecefc27221c960dae1432d0289f4d5
Helen <180019665+helenbun@users.noreply.github.com>
2024-09-09 06:47:19 +0100

    Completed javascript exercises 01-07.
    Some of my code was not as elegant as the solutions, but it all works.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let farenheit = (temp-32)* 5 / 9;
+  if (Number.isInteger(farenheit)) {
+    return farenheit
+  }
+  else {
+    farenheit = Number(farenheit.toFixed(1));
+    return farenheit;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let celsius = (temp * 9/5) + 32;
+  if (Number.isInteger(celsius)) {
+    return celsius;
+  }
+  else {
+    celsius = Number(celsius.toFixed(1));
+    return celsius;
+  }
 };
 
 // Do not edit below this line
@@ -9,3 +25,6 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+//(x-32)* 5/9
+//x × 9/5 + 32
