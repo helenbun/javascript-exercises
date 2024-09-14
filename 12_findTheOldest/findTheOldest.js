d09614cfc79acfb17510dcf4fbdf5f86cd94e818
Helen <180019665+helenbun@users.noreply.github.com>
2024-09-14 21:58:36 +0100

    Complete javascript exercises
    My code works, although it is often not as elegant as the solutions.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = 0;
+    people.forEach((person) => {
+        if ("yearOfDeath" in person == false) {
+            person["yearOfDeath"] = 2024;
+        }
+        person["age"] = person.yearOfDeath-person.yearOfBirth;
+        if (person.age > oldest) {
+            oldest = person.age;
+        }
+    });
+    let oldestPerson = (people.find(({ age }) => age === oldest));
+    return oldestPerson;
 };
 
 // Do not edit below this line
